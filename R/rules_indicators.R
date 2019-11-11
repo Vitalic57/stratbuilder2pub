4fb9cf4c0733a2c9d392ed26c7aaef1de277c4cb
Drastamat Kazanchyan <drastamat94@gmail.com>
2019-11-11 20:28:49 +0300

    additional functions were moved

--- R/rules_indicators.R
+++ R/rules_indicators.R
@@ -363,3 +363,24 @@ addIndicators.modelStrategy <- function(this, expr, names, as, lookback = 0, arg
 }
 
 
+
+quote_list <- function(args, env=NULL){
+    res <- list()
+    if(length(args) > 0){
+        nms <- names(args)
+        if(!is.null(nms)){
+            for(i in 1:length(args)){
+                if(nms[i] == ""){
+                    next
+                }
+                if(is.call(args[[i]]) && as.character(args[[i]][[1]]) == "quote" || is.symbol(args[[i]])){
+                    res[[nms[i]]] <- eval(args[[i]], envir = env)
+                }else{
+                    res[[nms[i]]] <- args[[i]]
+                }
+            }
+        }
+        
+    }
+    return(res)
+}
