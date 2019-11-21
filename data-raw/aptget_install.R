856aa578c5520ac7266691ffb9935d6aebf1b435
Vitaly <vitalic.s@financialracers.com>
2019-11-21 17:54:40 +0300

    delete raw

--- data-raw/aptget_install.R
+++
@@ -1,9 +0,0 @@
-f_inst <- function(x){
-  list.of.packages <- x
-  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-  if(length(new.packages)) install.packages(new.packages, repos = "http://cran.us.r-project.org")
-}
-f_inst(c('ssh', 'devtools'))
-devtools::document('/stratbuilder2pub')
-devtools::install('stratbuilder2pub')
-devtools::install_deps('/stratbuilder2pub')
--- data-raw/include_parts.R
+++
@@ -1,7 +0,0 @@
-# in our database there are several types of data
-# data_raw -- close price multiplied by lot size, shifted for futures
-# data_roll -- close price multiplied by lot size, no shift here
-# data_margin -- for now it equals to data_raw, but later it will be responsible for margin
-this$thisEnv$spreadData <- 'data_raw' # from what data spread will be built
-
-this$thisEnv$betaData <- 'data_raw' # what data will be passed to beta builder function
